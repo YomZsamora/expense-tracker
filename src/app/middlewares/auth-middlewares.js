4216f87385e9452a1e4d6a81d777b651e46efa37
yomZsamora <samaurah@gmail.com>
2026-08-16 06:11:42 +0300

    refactor: remove unused roleFieldValidator and clean up middleware exports

--- src/app/middlewares/auth-middlewares.js
+++ src/app/middlewares/auth-middlewares.js
@@ -1,57 +1,38 @@
 const { handleBadRequests } = require('../../utils/exceptions/exception-handler');
 const {
+    nameFieldValidator,
     emailFieldValidator,
     emailRegisteredValidator,
-    roleFieldValidator,
     registrationPasswordFieldValidator,
     passwordConfirmationFieldValidator,
     loginPasswordFieldValidator,
     emailExistsValidator,
     verifyPasswordValidator,
     refreshTokenCookieValidator,
-    refreshTokenExistsValidator
+    refreshTokenExistsValidator,
 } = require('../../utils/validators/auth-validators');
 
-/**
- * Middleware for validating basic registration requests.
- * This middleware checks the validity of the email and password fields,
- * ensures that the email is not already registered, and confirms that
- * the password and password confirmation match.
- */
 const basicRegistrationMiddleware = [
+    nameFieldValidator,
     emailFieldValidator,
     emailRegisteredValidator,
-    roleFieldValidator,
     registrationPasswordFieldValidator,
     passwordConfirmationFieldValidator,
-    handleBadRequests('Error occurred during registration.')
+    handleBadRequests('Error occurred during registration.'),
 ];
 
-/** * Middleware for validating basic login requests.
- * This middleware checks the validity of the email and password fields,
- * ensures that the email exists in the database, and verifies that
- * the provided password matches the stored password for that user.
- */
 const basicLoginMiddleware = [
     emailFieldValidator,
     loginPasswordFieldValidator,
     handleBadRequests('Error occurred during login.'),
     emailExistsValidator,
-    verifyPasswordValidator
+    verifyPasswordValidator,
 ];
 
-/** * Middleware for validating refresh token requests.
- * This middleware checks for the presence of a refresh token cookie,
- * verifies that the refresh token exists in the database and has not been revoked,
- * and handles any errors that may occur during these checks.
- */
-const refreshTokenMiddleware = [
-    refreshTokenCookieValidator,
-    refreshTokenExistsValidator
-];
+const refreshTokenMiddleware = [refreshTokenCookieValidator, refreshTokenExistsValidator];
 
-module.exports = { 
-    basicRegistrationMiddleware, 
+module.exports = {
+    basicRegistrationMiddleware,
     basicLoginMiddleware,
-    refreshTokenMiddleware
+    refreshTokenMiddleware,
 };
