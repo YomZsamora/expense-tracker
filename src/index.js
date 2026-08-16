3c5d81bae79afffd69d6358985e978c106b15c87
yomZsamora <samaurah@gmail.com>
2026-08-16 06:10:29 +0300

    refactor: reorganize middleware setup in index.js for improved readability

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const helmet = require('helmet');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const sequelize = require('./configs/sequelize');
+require('./configs/sequelize');
 const authRoutes = require('./app/routes/auth-routes');
 const { exceptionHandler } = require('./utils/exceptions/exception-handler');
 
@@ -9,10 +11,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
 app.get('/health', (req, res) => res.send('The expense tracker is running.'));
 app.use('/v1/auth/', authRoutes);
+
 app.use(exceptionHandler);
 
 if (require.main === module) {
