9379638d7b6fd73c214d0e785b8e9e925d957c08
yomZsamora <samaurah@gmail.com>
2026-09-01 07:30:09 +0300

    feat: add category serializer with serialization functions for single and list categories

---
+++ src/utils/serializers/category-serializer.js
@@ -0,0 +1,13 @@
+
+const serializeCategory = (category) => ({
+    id: category.id,
+    name: category.name,
+    type: category.type,
+    isDefault: category.isDefault,
+    createdAt: category.createdAt,
+    updatedAt: category.updatedAt,
+});
+
+const serializeCategoryList = (categories) => categories.map(serializeCategory);
+
+module.exports = { serializeCategory, serializeCategoryList };
