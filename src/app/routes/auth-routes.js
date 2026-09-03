7666815e91e0e1c5acbaab973e96c7f94fac7522
yomZsamora <samaurah@gmail.com>
2026-09-03 16:55:45 +0300

    feat: add authentication middleware to logout route for enhanced security

--- src/app/routes/auth-routes.js
+++ src/app/routes/auth-routes.js
@@ -1,21 +1,22 @@
 const express = require('express');
-const { 
-    basicRegistrationMiddleware, 
+const {
+    basicRegistrationMiddleware,
     basicLoginMiddleware,
     refreshTokenMiddleware
 } = require('../middlewares/authentication-middlewares');
-const { 
-    basicRegistrationController, 
+const {
+    basicRegistrationController,
     basicLoginController,
     refreshTokenController,
     logoutController
 } = require('../controllers/authentication-controllers.js');
+const { isUserAuthenticated } = require('../middlewares/authorization-middlewares');
 
 const router = express.Router();
 
 router.post('/register', basicRegistrationMiddleware, basicRegistrationController);
 router.post('/login', basicLoginMiddleware, basicLoginController);
 router.post('/refresh', refreshTokenMiddleware, refreshTokenController);
-router.post('/logout', logoutController);
+router.post('/logout', isUserAuthenticated, logoutController);
 
 module.exports = router;
