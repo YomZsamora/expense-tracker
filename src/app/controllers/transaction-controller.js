36948c6655e794ff26bc593a62cf7427f027d66d
yomZsamora <samaurah@gmail.com>
2026-09-08 20:51:36 +0300

    feat: implement create transaction controller

---
+++ src/app/controllers/transaction-controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { ApiResponse } = require('../../utils/responses');
+const transactionRepository = require('../../repositories/transaction-repository');
+const transactionSerializer = require('../../utils/serializers/transaction-serializer');
+
+const createTransactionController = async (req, res, next) => {
+    try {
+        const { amount, type, categoryId, date, description } = req.body;
+        const transaction = await transactionRepository.createTransaction({
+            userId: req.user.sub,
+            amount,
+            type,
+            categoryId,
+            date,
+            description,
+        });
+        const withCategory = await transactionRepository.findTransactionById(transaction.id);
+        const apiResponse = new ApiResponse();
+        apiResponse.message = 'Transaction created successfully.';
+        apiResponse.data = transactionSerializer.serializeTransaction(withCategory);
+        return res.status(201).json(apiResponse);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { createTransactionController };
