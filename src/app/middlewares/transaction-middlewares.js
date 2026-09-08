0c7d6b071a5cde60aee6c46e30bbae2ab453a205
yomZsamora <samaurah@gmail.com>
2026-09-08 20:51:45 +0300

    feat: add create transaction middleware with validation and error handling

---
+++ src/app/middlewares/transaction-middlewares.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { handleBadRequests } = require('../../utils/exceptions/exception-handler');
+const { isUserAuthenticated } = require('./authorization-middlewares');
+const {
+    amountFieldValidator,
+    typeFieldValidator,
+    categoryIdFieldValidator,
+    dateFieldValidator,
+    descriptionFieldValidator,
+    resolveCategoryForTransaction,
+} = require('../../utils/validators/transaction-validators');
+
+const createTransactionMiddleware = [
+    isUserAuthenticated,
+    amountFieldValidator,
+    typeFieldValidator,
+    categoryIdFieldValidator,
+    dateFieldValidator,
+    descriptionFieldValidator,
+    handleBadRequests('Error occurred while creating transaction.'),
+    resolveCategoryForTransaction,
+];
+
+module.exports = { createTransactionMiddleware };
