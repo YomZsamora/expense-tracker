7d4f0a9c9aa1b2d03c11c8c2e61b99a69fd816f9
yomZsamora <samaurah@gmail.com>
2026-09-09 19:46:07 +0300

    feat: add GET route for transaction retrieval with middleware and controller

--- src/app/routes/transaction-routes.js
+++ src/app/routes/transaction-routes.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const express = require('express');
-const { createTransactionMiddleware } = require('../middlewares/transaction-middlewares');
-const { createTransactionController } = require('../controllers/transaction-controller');
+const { createTransactionMiddleware, getTransactionMiddleware } = require('../middlewares/transaction-middlewares');
+const { createTransactionController, getTransactionController } = require('../controllers/transaction-controller');
 
 const router = express.Router();
 
 router.post('/', createTransactionMiddleware, createTransactionController);
+router.get('/:id', getTransactionMiddleware, getTransactionController);
 
 module.exports = router;
