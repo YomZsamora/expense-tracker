b75d2c87e075b8d879de57654839ff9c476759d1
yomZsamora <samaurah@gmail.com>
2026-09-09 20:51:18 +0300

    docs: add JSDoc comments for user authentication middleware

--- src/app/middlewares/authorization-middlewares.js
+++ src/app/middlewares/authorization-middlewares.js
@@ -3,6 +3,12 @@
 const { verifyAccessToken, isDenylisted } = require('../../services/token-service');
 const { NotAuthenticated } = require('../../utils/exceptions/custom-exceptions')
 
+/** * Checks if the user is authenticated.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next function.
+ * @returns {Promise<void>} - A promise that resolves to the authenticated user.
+ */
 const isUserAuthenticated = async (req, res, next) => {
 
     try {
