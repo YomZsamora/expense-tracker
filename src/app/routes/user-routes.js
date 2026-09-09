84013617c23343b7e872e1363095529a6a7f57a8
yomZsamora <samaurah@gmail.com>
2026-09-09 19:51:57 +0300

    chore: enable strict mode in user routes for improved reliability

--- src/app/routes/user-routes.js
+++ src/app/routes/user-routes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const { getUserMiddlewares } = require('../middlewares/user-middlewares');
 const { getUserController } = require('../controllers/user-controllers');
