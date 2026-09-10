2f6d6868b72892fd355ac8e7619de7a7eb0605d6
yomZsamora <samaurah@gmail.com>
2026-09-10 18:44:00 +0300

    refactor: remove unused getUserMiddlewares from user routes

--- src/app/routes/user-routes.js
+++ src/app/routes/user-routes.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const express = require('express');
-const { getUserMiddlewares } = require('../middlewares/user-middlewares');
 const { getUserController } = require('../controllers/user-controllers');
 
 const router = express.Router();
 
-router.get('/me', getUserMiddlewares, getUserController);
+router.get('/me', getUserController);
 
 module.exports = router;
