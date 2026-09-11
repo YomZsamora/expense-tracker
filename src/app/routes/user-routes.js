a03a35b1c78f87a241d7deae755011cae49dfc1e
yomZsamora <samaurah@gmail.com>
2026-09-11 13:45:32 +0300

    feat: add logout route to user routes for user logout functionality

--- src/app/routes/user-routes.js
+++ src/app/routes/user-routes.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const express = require('express');
-const { getUserController } = require('../controllers/user-controllers');
+const { 
+    getUserController,
+    logoutController
+} = require('../controllers/user-controllers');
 
 const router = express.Router();
 
 router.get('/me', getUserController);
+router.post('/logout', logoutController);
 
 module.exports = router;
