5fef2e13e813b6cfc6afc85adb3e6cfd05d51521
yomZsamora <samaurah@gmail.com>
2026-09-11 13:45:27 +0300

    refactor: remove logout route and associated middleware from auth routes

--- src/app/routes/auth-routes.js
+++ src/app/routes/auth-routes.js
@@ -10,15 +10,12 @@ const {
     basicRegistrationController,
     basicLoginController,
     refreshTokenController,
-    logoutController
 } = require('../controllers/authentication-controllers.js');
-const { isUserAuthenticated } = require('../middlewares/authorization-middlewares');
 
 const router = express.Router();
 
 router.post('/register', basicRegistrationMiddleware, basicRegistrationController);
 router.post('/login', basicLoginMiddleware, basicLoginController);
 router.post('/refresh', refreshTokenMiddleware, refreshTokenController);
-router.post('/logout', isUserAuthenticated, logoutController);
 
 module.exports = router;
