e6d73ee07cefd513e968176416edb4b4c2a3cfac
yomZsamora <samaurah@gmail.com>
2026-09-12 18:31:56 +0300

    feat: add PATCH route for transaction updates with middleware and controller integration

--- src/app/routes/transaction-routes.js
+++ src/app/routes/transaction-routes.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const express = require('express');
-const { createTransactionMiddleware, getTransactionMiddleware } = require('../middlewares/transaction-middlewares');
-const { createTransactionController, getTransactionController } = require('../controllers/transaction-controller');
+const { createTransactionMiddleware, getTransactionMiddleware, updateTransactionMiddleware } = require('../middlewares/transaction-middlewares');
+const { createTransactionController, getTransactionController, updateTransactionController } = require('../controllers/transaction-controller');
 
 const router = express.Router();
 
 router.post('/', createTransactionMiddleware, createTransactionController);
 router.get('/:id', getTransactionMiddleware, getTransactionController);
+router.patch('/:id', updateTransactionMiddleware, updateTransactionController);
 
 module.exports = router;
