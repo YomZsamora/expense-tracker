58ca57c485d938e7897b05a552ff19a41587f873
yomZsamora <samaurah@gmail.com>
2026-09-12 07:51:22 +0300

    test: remove user setup from test environment as it is no longer needed

--- src/tests/setupFilesAfterEnv.js
+++ src/tests/setupFilesAfterEnv.js
@@ -2,20 +2,6 @@
 
 const sequelize = require('../configs/sequelize');
 const redis = require('../configs/redis');
-const { User } = require('../models/user');
-const { faker } = require('@faker-js/faker');
-
-beforeAll(async () => {
-
-    await User.findOrCreate({
-        where: { email: 'test.user@example.com' },
-        defaults: {
-            name: faker.person.fullName(),
-            passwordHash: 'irrelevant',
-        },
-    });
-
-});
 
 afterAll(async () => {
     await sequelize.close();
