0bc7b67763da67f9a549fc7f74461870f08258fe
yomZsamora <samaurah@gmail.com>
2026-09-13 10:26:38 +0300

    feat: add DELETE endpoint for transaction removal with middleware and controller integration

--- src/app/routes/transaction-routes.js
+++ src/app/routes/transaction-routes.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const express = require('express');
-const { createTransactionMiddleware, getTransactionMiddleware, updateTransactionMiddleware } = require('../middlewares/transaction-middlewares');
-const { createTransactionController, getTransactionController, updateTransactionController } = require('../controllers/transaction-controller');
+const { createTransactionMiddleware, getTransactionMiddleware, updateTransactionMiddleware, deleteTransactionMiddleware } = require('../middlewares/transaction-middlewares');
+const { createTransactionController, getTransactionController, updateTransactionController, deleteTransactionController } = require('../controllers/transaction-controller');
 
 const router = express.Router();
 
 router.post('/', createTransactionMiddleware, createTransactionController);
 router.get('/:id', getTransactionMiddleware, getTransactionController);
 router.patch('/:id', updateTransactionMiddleware, updateTransactionController);
+router.delete('/:id', deleteTransactionMiddleware, deleteTransactionController);
 
 module.exports = router;
