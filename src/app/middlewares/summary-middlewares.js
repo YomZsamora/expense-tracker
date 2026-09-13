49f0dc3d2486c273c024799bdb1c05a42115e7f1
yomZsamora <samaurah@gmail.com>
2026-09-13 21:58:14 +0300

    feat: add trends middleware for validating monthly trends query parameters

--- src/app/middlewares/summary-middlewares.js
+++ src/app/middlewares/summary-middlewares.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const { handleBadRequests } = require('../../utils/exceptions/exception-handler');
-const { monthlySummaryQueryValidator } = require('../../utils/validators/summary-validators');
+const { monthlySummaryQueryValidator, trendsQueryValidator } = require('../../utils/validators/summary-validators');
 
-/** * Middleware for validating the monthly summary query parameters.
- * It uses the monthlySummaryQueryValidator to check the 'month' and 'year' query parameters.
- * If validation fails, it handles the bad requests with a specific error message.
- */
 const monthlySummaryMiddleware = [
     ...monthlySummaryQueryValidator,
     handleBadRequests('Error occurred while retrieving monthly summary.'),
 ];
 
-module.exports = { monthlySummaryMiddleware };
+const trendsMiddleware = [
+    ...trendsQueryValidator,
+    handleBadRequests('Error occurred while retrieving monthly trends.'),
+];
+
+module.exports = { monthlySummaryMiddleware, trendsMiddleware };
