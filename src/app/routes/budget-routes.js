06e26f80834b26a1ee344c5507c0246fd4e8c27e
yomZsamora <samaurah@gmail.com>
2026-09-13 16:31:28 +0300

    feat: add listBudgets route and middleware for budget retrieval

--- src/app/routes/budget-routes.js
+++ src/app/routes/budget-routes.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const express = require('express');
-const { createBudgetController, updateBudgetController, deleteBudgetController } = require('../controllers/budget-controller');
-const { createBudgetMiddleware, updateBudgetMiddleware, deleteBudgetMiddleware } = require('../middlewares/budget-middlewares');
+const { listBudgetsController, createBudgetController, updateBudgetController, deleteBudgetController } = require('../controllers/budget-controller');
+const { listBudgetsMiddleware, createBudgetMiddleware, updateBudgetMiddleware, deleteBudgetMiddleware } = require('../middlewares/budget-middlewares');
 
 const router = express.Router();
 
+router.get('/', listBudgetsMiddleware, listBudgetsController);
 router.post('/', createBudgetMiddleware, createBudgetController);
 router.patch('/:id', updateBudgetMiddleware, updateBudgetController);
 router.delete('/:id', deleteBudgetMiddleware, deleteBudgetController);
