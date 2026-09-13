6f92ff1b99e0464d903e36c80fac1c24af2e97f8
yomZsamora <samaurah@gmail.com>
2026-09-13 11:31:10 +0300

    feat: integrate listTransactionsMiddleware and listTransactionsController into transaction routes for transaction listing functionality

--- src/app/routes/transaction-routes.js
+++ src/app/routes/transaction-routes.js
@@ -1,12 +1,25 @@
 'use strict';
 
 const express = require('express');
-const { createTransactionMiddleware, getTransactionMiddleware, updateTransactionMiddleware, deleteTransactionMiddleware } = require('../middlewares/transaction-middlewares');
-const { createTransactionController, getTransactionController, updateTransactionController, deleteTransactionController } = require('../controllers/transaction-controller');
+const { 
+    createTransactionMiddleware, 
+    getTransactionMiddleware, 
+    updateTransactionMiddleware, 
+    deleteTransactionMiddleware, 
+    listTransactionsMiddleware 
+} = require('../middlewares/transaction-middlewares');
+const { 
+    createTransactionController, 
+    getTransactionController, 
+    updateTransactionController, 
+    deleteTransactionController, 
+    listTransactionsController 
+} = require('../controllers/transaction-controller');
 
 const router = express.Router();
 
 router.post('/', createTransactionMiddleware, createTransactionController);
+router.get('/', listTransactionsMiddleware, listTransactionsController);
 router.get('/:id', getTransactionMiddleware, getTransactionController);
 router.patch('/:id', updateTransactionMiddleware, updateTransactionController);
 router.delete('/:id', deleteTransactionMiddleware, deleteTransactionController);
