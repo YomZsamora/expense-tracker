b432b667d18ea17d6c70e0d0bfca72774ef4db4b
yomZsamora <samaurah@gmail.com>
2026-09-13 16:25:18 +0300

    feat: add deleteBudget route and middleware for budget deletion

--- src/app/routes/budget-routes.js
+++ src/app/routes/budget-routes.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const express = require('express');
-const { createBudgetController, updateBudgetController } = require('../controllers/budget-controller');
-const { createBudgetMiddleware, updateBudgetMiddleware } = require('../middlewares/budget-middlewares');
+const { createBudgetController, updateBudgetController, deleteBudgetController } = require('../controllers/budget-controller');
+const { createBudgetMiddleware, updateBudgetMiddleware, deleteBudgetMiddleware } = require('../middlewares/budget-middlewares');
 
 const router = express.Router();
 
 router.post('/', createBudgetMiddleware, createBudgetController);
 router.patch('/:id', updateBudgetMiddleware, updateBudgetController);
+router.delete('/:id', deleteBudgetMiddleware, deleteBudgetController);
 
 module.exports = router;
