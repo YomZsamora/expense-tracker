1d5f237806e190458435b87bbd8099381e0eca12
yomZsamora <samaurah@gmail.com>
2026-09-13 15:28:51 +0300

    feat: establish associations between Budget, User, and Category models for enhanced data relationships

--- src/models/associations.js
+++ src/models/associations.js
@@ -1,11 +1,30 @@
 const { Transaction } = require('./transaction');
 const { Category } = require('./category');
+const { Budget } = require('./budget');
+const { User } = require('./user');
 
 Transaction.belongsTo(Category, {
     foreignKey: 'categoryId',
     as: 'category'
 });
-Category.hasMany(Transaction,   {
+Category.hasMany(Transaction, {
     foreignKey: 'categoryId',
     as: 'transactions'
+});
+
+Budget.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'user',
+});
+Budget.belongsTo(Category, {
+    foreignKey: 'categoryId',
+    as: 'category',
+});
+User.hasMany(Budget, {
+    foreignKey: 'userId',
+    as: 'budgets',
+});
+Category.hasMany(Budget, {
+    foreignKey: 'categoryId',
+    as: 'budgets',
 });
