13a69c26b30e4fed821664ee21dc77fc7e5d07e5
yomZsamora <samaurah@gmail.com>
2026-09-14 06:13:55 +0300

    feat: add categories route to retrieve category breakdown data with associated middleware

--- src/app/routes/summary-routes.js
+++ src/app/routes/summary-routes.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const express = require('express');
-const { getMonthlySummaryController, getMonthlyTrendsController } = require('../controllers/summary-controller');
-const { monthlySummaryMiddleware, trendsMiddleware } = require('../middlewares/summary-middlewares');
+const { getMonthlySummaryController, getMonthlyTrendsController, getCategoryBreakdownController } = require('../controllers/summary-controller');
+const { monthlySummaryMiddleware, trendsMiddleware, categoryBreakdownMiddleware } = require('../middlewares/summary-middlewares');
 
 const router = express.Router();
 
 router.get('/monthly', monthlySummaryMiddleware, getMonthlySummaryController);
 router.get('/trends', trendsMiddleware, getMonthlyTrendsController);
+router.get('/categories', categoryBreakdownMiddleware, getCategoryBreakdownController);
 
 module.exports = router;
